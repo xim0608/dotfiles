d038b1ab04d646a1921847ed814df6eece314510
xim0608 <xim.ryuki@gmail.com>
2018-04-02 14:45:09 +0900

    fix ndenv install and gatsby

--- cookbooks/ndenv/default.rb
+++ cookbooks/ndenv/default.rb
@@ -1,6 +1,6 @@
 package 'ndenv'
 
-ndenv_root = "/#{ENV['USER']}/.ndenv/plugins/node-build"
+ndenv_root = "/Users/#{ENV['USER']}/.ndenv/plugins/node-build"
 git ndenv_root do
   repository 'https://github.com/riywo/node-build.git'
   not_if { Dir.exists?(ndenv_root)}
@@ -9,6 +9,26 @@ end
 execute 'reloading shell' do
   user ENV['USER']
   command 'source ~/.zshenv'
+end
+
+node_version = 'v8.11.1'
+execute 'install node' do
+  user ENV['USER']
+  command "ndenv install #{node_version}"
+  not_if {
+    result = run_command('ndenv versions', error: false)
+    result.stdout.include?(node_version)
+  }
+end
+
+execute 'rehash ndenv' do
+  user ENV['USER']
+  command 'ndenv rehash'
+end
+
+execute 'global ndenv installed version' do
+  user ENV['USER']
+  command "ndenv global #{node_version}"
 end
 
 execute 'install gatsbyjs' do
