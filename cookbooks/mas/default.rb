5ffa585f5539928351082dfba08d2f79da7438b1
xim0608 <xim.ryuki@gmail.com>
2018-04-30 10:45:55 +0900

    modify mas script and list

--- cookbooks/mas/default.rb
+++ cookbooks/mas/default.rb
@@ -2,5 +2,12 @@ mas_json = File.read("./cookbooks/mas/files/#{node[:os]}.json")
 node.reverse_merge!(JSON.load(mas_json))
 
 node['mas_ids'].each do |mas_id|
-  execute "mas install #{mas_id}"
+  execute "install package" do
+    user ENV['USER']
+    command "mas install #{mas_id}"
+    not_if {
+      result = run_command("mas list | grep #{mas_id}")
+      result.exit_status == 0
+    }
+  end
 end
