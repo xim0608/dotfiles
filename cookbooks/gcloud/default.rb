ea94af25745d9cdf0e03ac7377b0b7fcf3d0a98d
xim0608 <xim.ryuki@gmail.com>
2018-05-08 16:58:26 +0900

    fix typo and condition

--- cookbooks/gcloud/default.rb
+++ cookbooks/gcloud/default.rb
@@ -4,17 +4,19 @@ base_target_dir = "$HOME"
 shell_dir = "$HOME/google-cloud-sdk"
 shell_file = "#{shell_dir}/install.sh"
 
-execute "download gloud sdk files" do
+execute "download gcloud sdk files" do
   command "curl -o #{tar_file_name} -fL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-192.0.0-darwin-x86_64.tar.gz"
   not_if {
-    File.exists?(tar_file_name)
+    result = run_command("type gcloud", error: false)
+    File.exists?(tar_file_name) || result.exit_status == 0
   }
 end
 
 execute "extract and remove gcloud-sdk.tar.gz" do
   command "tar zxvf #{tar_file_name} -C $HOME && rm #{tar_file_name}"
   not_if {
-    File.exists?("#{base_target_dir}/google-cloud-sdk/install.sh")
+    result = run_command("type gcloud", error: false)
+    File.exists?("#{base_target_dir}/google-cloud-sdk/install.sh") || result.exit_status == 0
   }  
 end
 
